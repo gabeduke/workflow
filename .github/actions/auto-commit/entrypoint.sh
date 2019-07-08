c3f67be96964d823f3dada0ef8b9d1cd3e797c2c
gabeduke <gabeduke@gmail.com>
2019-07-07 21:15:03 -0400

    stash

--- .github/actions/auto-commit/entrypoint.sh
+++ .github/actions/auto-commit/entrypoint.sh
@@ -5,7 +5,9 @@ BRANCH=develop
 
 git config --global user.name 'autobot'
 git config --global user.email 'autobot@leetserve.com'
+git stash
 git checkout "${BRANCH}"
 git pull
+git stash pop
 git add -A && git commit -m 'bumpver' --allow-empty
 git push -u origin "${BRANCH}"
